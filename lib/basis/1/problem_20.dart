196084524b376b62bb78dbfea2e7a0d4eee1aa49
Sergei Sorokin <sench32@gmail.com>
2021-08-30 22:33:15 +0300

    added solution code "problem 20"

---
+++ lib/basis/1/problem_20.dart
@@ -0,0 +1,34 @@
+
+void main() {
+  String resultStr = '100';
+  String factorStr = '';
+  String resultFactorStr = '';
+  int factor = 99;
+  int factor1 = 0;
+  int factor2 = 0;
+  int temp = 0;
+  int parse = 0;
+  while (factor > 1) {
+    for (var count = resultStr.length; count >= 0; count--) {
+      if (count > 0) {
+        parse = int.parse(resultStr[count - 1]);
+        factor1 = (factor * parse) + temp;
+        factorStr = (factor1 % 10).toString();
+        resultFactorStr = factorStr + resultFactorStr;
+        temp = factor1 % 1000 ~/ 10;
+      } else {
+        resultFactorStr = temp.toString() + resultFactorStr;
+        temp = 0;
+      }
+    }
+    resultStr = resultFactorStr;
+    resultFactorStr = '';
+    factor--;
+  }
+  print(resultStr);
+  for (var count = resultStr.length; count > 0; count--){
+    parse = int.parse(resultStr[count - 1]);
+    factor2 = factor2 + parse;
+  }
+  print(factor2);
+}
