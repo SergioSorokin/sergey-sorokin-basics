da35cf120a3ef0f2edbc07a34d76c25c2283cfbf
Sergei Sorokin <sergei.sorokin@itomy.ch>
2021-10-05 19:43:54 +0300

    implemented the calculation of the three-digit factorial with display in the UI

--- lib/basis/1/18_factorial.dart
+++ lib/basis/1/18_factorial.dart
@@ -43,86 +43,91 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Form(
         key: formKey,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 50, bottom: 8),
-              child: Text('Введите число '),
-            ),
-            Center(
-              child: Container(
-                width: 80,
-                height: 60,
-                child: TextFormField(
-                  textAlign: TextAlign.center,
-                  maxLength: 3,
-                  onChanged: (value) {
-                    setState(
-                      () {
-                        inputNumber = numberController.text;
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50, bottom: 8),
+                  child: Text('Введите число '),
+                ),
+                Center(
+                  child: Container(
+                    width: 80,
+                    height: 60,
+                    child: TextFormField(
+                      textAlign: TextAlign.center,
+                      maxLength: 3,
+                      onFieldSubmitted: validator,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            inputNumber =
+                                int.parse(numberController.text).toString();
+                          },
+                        );
                       },
-                    );
-                  },
-                  controller: numberController,
-                  decoration: InputDecoration(
-                    hintText: '',
-                    hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
+                      controller: numberController,
+                      decoration: InputDecoration(
+                        hintText: '',
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          setState(
+                            () {
+                              factorial = 'Глупо проверять пустое поле';
+                            },
+                          );
+                          return '';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(
-                      RegExp(r'[0-9]'),
-                    )
-                  ],
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      setState(
-                        () {
-                          factorial = 'Глупо проверять пустое поле';
-                        },
-                      );
-                      return '';
-                    }
-                    return null;
-                  },
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 50, bottom: 8),
-              child: Text(
-                  'Факториал числа $inputNumber${inputNumber == '' ? inputNumber : symbol}'),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 28, bottom: 8),
-              child: Text(
-                factorial,
-                style: TextStyle(color: Colors.pink, fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Padding(
-        padding: EdgeInsets.all(16),
-        child: ConstrainedBox(
-          constraints:
-              const BoxConstraints(minWidth: double.maxFinite, minHeight: 48),
-          child: ElevatedButton(
-            onPressed: validator,
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            child: Text(
-              'Узнать факториал числа $inputNumber',
-              style: const TextStyle(fontSize: 14),
+                Padding(
+                  padding: const EdgeInsets.only(top: 50, bottom: 8),
+                  child: Text(
+                      'Факториал числа $inputNumber${inputNumber == '' ? inputNumber : symbol}'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 28, bottom: 8),
+                  child: Text(
+                    factorial,
+                    style: TextStyle(color: Colors.pink, fontSize: 18),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                        minWidth: double.maxFinite, minHeight: 48),
+                    child: ElevatedButton(
+                      onPressed: () => validator(inputNumber),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: Text(
+                        'Узнать факториал числа $inputNumber',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -130,18 +135,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void validator() {
+  void validator(String inputNumber) {
     if (formKey.currentState!.validate()) {
       for (var count = inputNumber.length; count > 0; count--) {
         parseNumber = int.parse(inputNumber);
       }
       setState(
         () {
-          factorial = '';
-          if (parseNumber == 0) {
-            factorial = '1';
-          }
-          factorialCalculation();
+          factorial = factorialCalculation(inputNumber);
         },
       );
     }
@@ -153,16 +154,48 @@ class _HomePageState extends State<HomePage> {
     numberController.dispose();
   }
 
-  factorialCalculation() {
-    while (countFC <= parseNumber) {
-      store = (store * countFC);
-      factorial = store.toString();
-      countFC++;
-      print(factorial);
-      print(parseNumber);
+  String factorialCalculation(String inputNumber) {
+    String factorStr = '';
+    String resultFactorStr = '';
+    String factorial = '';
+    int parseNumber = 0;
+
+    int factor = 0;
+    int factor1 = 0;
+    int temp = 0;
+    int parse = 0;
+
+    parseNumber = int.parse(inputNumber);
+    factor = parseNumber - 1;
+
+    while (factor > 1) {
+      for (var count = inputNumber.length; count >= 0; count--) {
+        if (count > 0) {
+          parse = int.parse(inputNumber[count - 1]);
+          factor1 = (factor * parse) + temp;
+          factorStr = (factor1 % 10).toString();
+          resultFactorStr = factorStr + resultFactorStr;
+          temp = factor1 % 1000 ~/ 10;
+        } else {
+          if (temp != 0) {
+            resultFactorStr = temp.toString() + resultFactorStr;
+          }
+
+          temp = 0;
+        }
+      }
+      if (resultFactorStr[0] == '0' && resultFactorStr.length > 1) {
+        inputNumber = resultFactorStr.substring(1);
+      }
+      inputNumber = resultFactorStr;
+      resultFactorStr = '';
+      factor--;
     }
-    // print(factorial);
-    store = 1;
-    countFC = 1;
+    if (inputNumber == '0') {
+      factorial = '1';
+    } else {
+      factorial = inputNumber;
+    }
+    return factorial;
   }
 }
