0be6a1b8c0b82ab1ab28b45ce382ad4a8049f9c2
Sergei Sorokin <sergei.sorokin@itomy.ch>
2021-11-03 21:18:49 +0200

    SU-3722: implement a constructor and function parseInputString in process

--- lib/basis/2/special_type_money.dart
+++ lib/basis/2/special_type_money.dart
@@ -1,1 +1,30 @@
-class MyMoney {}
+class MyMoney {
+  late final int dollars;
+  late final int cents;
+
+  List<String> listInputString = [];
+
+  MyMoney({required String inputString}) {
+    parseInputString(inputString);
+  }
+
+  void parseInputString(String inputString) {
+    listInputString = inputString.split('.');
+    if (listInputString[0].isNotEmpty || listInputString.length == 1) {
+      dollars = int.parse(listInputString[0]);
+    } else {
+      dollars = 0;
+    }
+    if (listInputString.length == 2 && listInputString[1].isNotEmpty) {
+      cents = int.parse(listInputString[1]);
+    } else {
+      cents = 0;
+    }
+  }
+
+  @override
+  String toString() {
+    var centStr = cents == 0 ? '00' : cents.toString();
+    return '$dollars \$  $centStr ¢' '';
+  }
+}
