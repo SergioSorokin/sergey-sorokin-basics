ac746191f144a369b99f2afd663dfef4b1934826
Sergei Sorokin <sergei.sorokin@itomy.ch>
2021-11-05 16:59:03 +0200

    SU-3722: overflow bugs fix and variable renaming

--- lib/basis/2/special_type_money.dart
+++ lib/basis/2/special_type_money.dart
@@ -2,53 +2,61 @@ class MyMoney {
   late final int dollars;
   late final int cents;
 
-  List<String> listInputString = [];
+  List<String> _listInputString = ['', ''];
 
   MyMoney({required String inputString}) {
-    parseInputString(inputString);
+    _parseInputString(inputString);
   }
 
-  void parseInputString(String inputString) {
-    listInputString = inputString.split('.');
+  void _parseInputString(String inputString) {
+    if (inputString.contains('.')) {
+      _listInputString = inputString.split('.');
+    } else {
+      _listInputString[0] = inputString;
+      _listInputString[1] = '0';
+    }
     var hasOverflow = false;
-    if (listInputString.length > 1 && listInputString[1].isNotEmpty) {
-      if (listInputString[1].length == 1) {
-        cents = int.parse(listInputString[1]) * 10;
-      } else if (listInputString[1].length > 2) {
-        cents = overflow(listInputString);
+    if (_listInputString.length > 0 && _listInputString[1].isNotEmpty) {
+      if (_listInputString[1].length == 1) {
+        cents = int.parse(_listInputString[1]) * 10;
+      } else if (_listInputString[1].length > 2) {
+        cents = _overflow(_listInputString);
         if (cents == 0) {
           hasOverflow = true;
         }
       } else {
-        cents = int.parse(listInputString[1]);
+        cents = int.parse(_listInputString[1]);
       }
     } else {
       cents = 0;
     }
-    if (listInputString[0].isEmpty) {
+    if (_listInputString[0].isEmpty) {
       if (hasOverflow) {
-        listInputString[0] = '0';
-        dollars = int.parse(listInputString[0]) + 1;
+        _listInputString[0] = '0';
+        dollars = int.parse(_listInputString[0]) + 1;
       } else {
         dollars = 0;
       }
     } else {
       dollars = hasOverflow
-          ? int.parse(listInputString[0]) + 1
-          : int.parse(listInputString[0]);
+          ? int.parse(_listInputString[0]) + 1
+          : int.parse(_listInputString[0]);
     }
   }
 
-  int overflow(List<String> listInputString) {
+  int _overflow(List<String> listInputString) {
     var cents;
     int parseInputNum =
         int.parse(listInputString[0] + listInputString[1].substring(0, 2));
     var temp = int.parse(listInputString[1].substring(2, 3));
     if (temp >= 5) {
       parseInputNum = parseInputNum + 1;
-      String testString = parseInputNum.toString();
-      cents = int.parse(
-          testString.substring(testString.length - 2, testString.length));
+      String tempString = parseInputNum.toString();
+      cents = tempString.length > 1
+          ? int.parse(
+              tempString.substring(tempString.length - 2, tempString.length))
+          : int.parse(
+              tempString.substring(tempString.length - 1, tempString.length));
     } else {
       cents = int.parse(listInputString[1].substring(0, 2));
     }
@@ -57,7 +65,12 @@ class MyMoney {
 
   @override
   String toString() {
-    var centStr = cents == 0 ? '00' : cents.toString();
-    return '$dollars \$  $centStr ¢' '';
+    var centStr;
+    if (cents.toString().length < 2) {
+      centStr = '0$cents';
+    } else {
+      centStr = cents == 0 ? '00' : cents.toString();
+    }
+    return '$dollars \$  $centStr ¢';
   }
 }
